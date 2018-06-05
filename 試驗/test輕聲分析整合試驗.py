67817bb9008026207b733fd030fd99e3de39cb9d
a8568730 <a8568730@hotmail.com>
2018-06-05 17:10:16 +0800

    輕聲分析整合試驗有三个試驗。

--- 試驗/test輕聲分析整合試驗.py
+++ 試驗/test輕聲分析整合試驗.py
@@ -4,9 +4,38 @@ from 輕聲分析.分析 import 輕聲分析
 
 
 class 輕聲分析整合試驗(TestCase):
-    def test愛斷開的輕聲詞(self):
-        參數 = 拆文分析器.對齊句物件('靚喔', 'tsiâng--oh')
-        按算結果 = 拆文分析器.對齊句物件('靚喔', 'tsiâng --oh')
+    def test一个kap頭前(self):
+        # A --B --C =>
+        # A--B, --C
+        漢字 = '林家啦'
+        原臺羅 = 'Lîm --ka --lah'
+        按算臺羅 = 'Lîm--ka --lah'
+        參數 = 拆文分析器.對齊句物件(漢字, 原臺羅)
+        按算結果 = 拆文分析器.對齊句物件(漢字, 按算臺羅)
+        self.assertEqual(
+            輕聲分析(參數), 按算結果
+        )
+
+    def test斷開一个(self):
+        # A --B--C =>
+        # A, --B, --C
+        漢字 = '林先生啦'
+        原臺羅 = 'Lîm --sian-sinn--lah'
+        按算臺羅 = 'Lîm --sian-sinn --lah'
+        參數 = 拆文分析器.對齊句物件(漢字, 原臺羅)
+        按算結果 = 拆文分析器.對齊句物件(漢字, 按算臺羅)
+        self.assertEqual(
+            輕聲分析(參數), 按算結果
+        )
+
+    def test斷開兩个(self):
+        # A--B-C--D E =>
+        # A, B-C, D, E
+        漢字 = '照顧一下啦你'
+        原臺羅 = 'tsiàu-kòo--tsi̍t-ē--lah lí'
+        按算臺羅 = 'tsiàu-kòo --tsi̍t-ē --lah lí'
+        參數 = 拆文分析器.對齊句物件(漢字, 原臺羅)
+        按算結果 = 拆文分析器.對齊句物件(漢字, 按算臺羅)
         self.assertEqual(
             輕聲分析(參數), 按算結果
         )
