bf6b64e5b81597aa2b01cb25bd3740a4708901f2
a8568730 <a8568730@hotmail.com>
2018-06-06 08:34:57 +0800

    ē-sái拆開詞內輕聲。

--- 試驗/test拆開詞內輕聲單元試驗.py
+++ 試驗/test拆開詞內輕聲單元試驗.py
@@ -1,46 +1,55 @@
 from unittest.case import TestCase
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.基本物件.詞 import 詞
-from 輕聲分析.分析 import 拆開詞內輕聲
+from 臺灣言語工具.基本物件.字 import 字
+from 輕聲分析.分析 import 輕聲分析器
 
 
 class 拆開詞內輕聲單元試驗(TestCase):
 
     def test無輕聲詞(self):
         參數 = 拆文分析器.對齊詞物件('照顧', 'tsiàu-kòo')
+        分析器物件 = 輕聲分析器()
         self.assertEqual(
-            len(拆開詞內輕聲(參數)), 1
+            len(分析器物件.拆開詞內輕聲(參數)), 1
         )
 
     def test攏總兩个詞(self):
         參數 = 拆文分析器.對齊詞物件('靚喔', 'tsiâng--ooh')
+        分析器物件 = 輕聲分析器()
         self.assertEqual(
-            len(拆開詞內輕聲(參數)), 2
+            len(分析器物件.拆開詞內輕聲(參數)), 2
         )
 
     def test一个輕聲孤字詞(self):
         參數 = 拆文分析器.對齊詞物件('靚喔', 'tsiâng--ooh')
+        分析器物件 = 輕聲分析器()
+        結果喔 = 分析器物件.拆開詞內輕聲(參數)[1]
         self.assertEqual(
-            拆開詞內輕聲(參數)[1],
-            詞('喔', '--ooh')
+            結果喔,
+            [字('喔', 'ooh')]
         )
 
-    def test一个輕聲雙字詞(self):
-        參數 = 拆文分析器.對齊詞物件('照顧一下', 'tsiàu-kòo--tsi̍t-ē')
+    def test有保留輕聲標記(self):
+        參數 = 拆文分析器.對齊詞物件('靚喔', 'tsiâng--ooh')
+        分析器物件 = 輕聲分析器()
+        結果喔 = 分析器物件.拆開詞內輕聲(參數)[1]
         self.assertEqual(
-            拆開詞內輕聲(參數)[1],
-            詞('一下', '--tsi̍t-ē')
+            結果喔[0].敢有輕聲標記(),
+            True
         )
-
+        
     def test兩个輕聲詞(self):
         參數 = 拆文分析器.對齊詞物件('靚一下一下', 'tsiâng--tsi̍t-ē--tsi̍t-ē')
+        分析器物件 = 輕聲分析器()
         self.assertEqual(
-            拆開詞內輕聲(參數)[1],
-            詞('一下', '--tsi̍t-ē')
+            分析器物件.拆開詞內輕聲(參數)[-1],
+            [字('一', 'tsi̍t'), 字('下', 'ē')]
         )
 
     def test攏總三个詞(self):
         參數 = 拆文分析器.對齊詞物件('靚一下一下', 'tsiâng--tsi̍t-ē--tsi̍t-ē')
+        分析器物件 = 輕聲分析器()
         self.assertEqual(
-            len(拆開詞內輕聲(參數)), 3
+            len(分析器物件.拆開詞內輕聲(參數)), 3
         )
--- 試驗/test讀輕聲詞清單單元試驗.py
+++ 試驗/test讀輕聲詞清單單元試驗.py
@@ -4,9 +4,9 @@ from 輕聲分析.輕聲詞清單 import 讀輕聲詞清單
 
 class 讀輕聲詞清單單元試驗(TestCase):
     def test讀著愛拆開的詞(self):
-        拆開清單 = 讀輕聲詞清單()[0]
+        拆開清單 = 讀輕聲詞清單()['拆開清單']
         self.assertIn('我｜guá', 拆開清單)
     
     def test讀著愛連寫的詞(self):
-        連寫清單 = 讀輕聲詞清單()[1]
+        連寫清單 = 讀輕聲詞清單()['連寫清單']
         self.assertIn('媽｜mah', 連寫清單)
