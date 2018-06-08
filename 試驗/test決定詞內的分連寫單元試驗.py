13dfd9dab7338d171775e0504fb3545ab50b26bd
a8568730 <a8568730@hotmail.com>
2018-06-08 12:34:35 +0800

    決定詞內分連寫，補試衍生詞--來去靚的判斷。

--- 試驗/test決定詞內的分連寫單元試驗.py
+++ 試驗/test決定詞內的分連寫單元試驗.py
@@ -1,6 +1,7 @@
 from unittest.case import TestCase
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 輕聲分析.分析 import 輕聲分析器
+from unittest.mock import patch
 
 
 class 決定詞內的分連寫單元試驗(TestCase):
@@ -11,7 +12,9 @@ class 決定詞內的分連寫單元試驗(TestCase):
             分析器物件.決定詞內的分連寫(一詞, self.輕聲位),
             self.按算結果
         )
-
+    
+    # 判斷清單內底有收的詞
+    
     def test分寫的輕聲詞(self):
         self.漢字 = '林啦'
         self.臺羅 = 'Lîm--lah'
@@ -42,8 +45,50 @@ class 決定詞內的分連寫單元試驗(TestCase):
         self.輕聲位 = []
         self.按算結果 = []
 
-    def test清單收__一嘛愛判斷著衍生詞__一下(self):
+    def test清單衍生詞__一下(self):
         self.漢字 = '靚一下一下'
         self.臺羅 = 'tsiâng--tsi̍t-ē--tsi̍t-ē'
         self.輕聲位 = [1, 3]
         self.按算結果 = [True, True]
+
+    @patch('輕聲分析.輕聲詞清單.輕聲詞清單.讀輕聲詞清單')
+    def test清單愛先對著較長的收詞__來去媠(self, 清單mock):
+        清單mock.return_value = [
+            ['來-去｜lâi-khì'],  # 拆開清單
+            ['來｜lâi']  # 連寫清單
+        ]
+        self.漢字 = '林來去靚'
+        self.臺羅 = 'Lîm--lâi-khì-tsiâng'
+        self.輕聲位 = [1]
+        self.按算結果 = [True]
+
+    @patch('輕聲分析.輕聲詞清單.輕聲詞清單.讀輕聲詞清單')
+    def test清單對著較短的收詞__來靚(self, 清單mock):
+        清單mock.return_value = [
+            ['來-去｜lâi-khì'],  # 拆開清單
+            ['來｜lâi']  # 連寫清單
+        ]
+        self.漢字 = '林來靚'
+        self.臺羅 = 'Lîm--lâi-tsiâng'
+        self.輕聲位 = [1]
+        self.按算結果 = [False]
+    
+    # 判斷清單內底無收的詞
+    
+    def test清單無收就維持連寫林__靚(self):
+        self.漢字 = '林靚'
+        self.臺羅 = 'Lîm--tsiâng'
+        self.輕聲位 = [0, ]
+        self.按算結果 = [False]
+
+    def test清單無收這个輕聲詞__靚(self):
+        self.漢字 = '靚'
+        self.臺羅 = '--tsiâng'
+        self.輕聲位 = [0, ]
+        self.按算結果 = [False]
+
+    def test清單無收遮的輕聲詞__靚(self):
+        self.漢字 = '靚靚'
+        self.臺羅 = '--tsiâng--tsiâng'
+        self.輕聲位 = [0, 1]
+        self.按算結果 = [False, True]
