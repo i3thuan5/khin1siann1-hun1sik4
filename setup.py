b96aa2a31a8b5d13e3438bccda8210cadd6468a8
pigu <a8568730@gmail.com>
2018-06-12 17:08:25 +0800

    ti7輕聲詞資料加__init__.py，就會使用wheel壓縮--矣。

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 from setuptools import setup, find_packages
-from os.path import os
 
 版本 = '1.0.0'
-
-def 揣工具包(頭='.'):
-    'setup的find_packages無支援windows中文檔案'
-    工具包 = []
-    for 目錄, _, 檔案 in os.walk(頭):
-        if '__init__.py' in 檔案:
-            工具包.append(目錄.replace('/', '.'))
-    return 工具包
 
 setup(
     name='khin1siann1_hun1sik4',
     version=版本,
-#     packages = 揣工具包('輕聲分析'),
-    packages = find_packages(exclude=["試驗"]),
+    packages=find_packages(),
     install_requires=['tai5-uan5_gian5-gi2_kang1-ku7'],
     include_package_data=True,
+    
     author='意傳科技',
     author_email='a8568730@gmail.com',
     description='計算詞頻時的輕聲分析工具',
